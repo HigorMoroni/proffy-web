ea2c67072909b9d93453aca02efabb7791fdd09a
Higor M. G. Ferreira <moroni.higor@gmail.com>
2020-08-06 19:52:52 -0300

    :mag: Search Teacher implementado

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -4,30 +4,37 @@ import './styles.css';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
-function TeacherItem() {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+};
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
   return (
     <article className="teacher-item">
       <header>
-        <img
-          src="https://avatars2.githubusercontent.com/u/25966584?s=460&u=e94d28fc7b8f64ff430191cb30199b62357a71c5&v=4"
-          alt="Higor Moroni"
-        />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>Higor Moroni</strong>
-          <span>Matemática</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
-      <p>
-        Entusiasta das melhores tecnologias de matemática avançada.
-        <br/><br/>
-        Apaixonado por calculos e por mudar a vida das pessoas através da matemática...
-      </p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 200,00</strong>
+          <strong>R$ {teacher.cost.toFixed(2).replace('.',',')}</strong>
         </p>
         <button type="button">
           <img src={whatsappIcon} alt="Whatsapp"/>
