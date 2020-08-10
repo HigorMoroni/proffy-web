4692b74888ec057a69903077a5510621c55bb845
Higor Moroni G. Ferreira <moroni@higor.tech>
2020-08-10 18:57:17 -0300

    Update index.tsx

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -42,7 +42,7 @@ const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
       <footer>
         <p>
           Preço/hora
-          <strong>R$ {teacher.cost},00</strong>
+          <strong>R$ {teacher.cost.replace('.',','}</strong>
         </p>
         <a href={`https://wa.me/${teacher.whatsapp}`}
           target="_blank"
